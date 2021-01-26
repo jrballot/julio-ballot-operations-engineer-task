ad4c7c7ca2167cccd70f27cfdc8aea7eea269d80
Júlio Ballot <julio.ballot@gmail.com>
2021-01-26 10:55:29 -0300

    Adding the rds.tf file with the AWS RDS configurations for a PostgreSQL database
    This RDS database is runing a postgres engine, with multiaz setted to
    false and no snapshot. It also has two subnets, one public and the other
    private so the aplication could comunicate with the database without
    compromissing the data during access.

---
+++ rds.tf
@@ -0,0 +1,24 @@
+resource "aws_db_parameter_group" "main" {
+  name   = "postgresql"
+  family = "postgres9.6"
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = [aws_subnet.private.id, aws_subnet.public.id]
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = var.rds_disk_size
+  storage_type         = "gp2"
+  engine               = var.rds_engine
+  engine_version       = var.rds_engine_version
+  instance_class       = var.rds_instance_class
+  name                 = var.rds_database_name
+  username             = var.rds_username
+  password             = var.rds_password
+  parameter_group_name = aws_db_parameter_group.main.name
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  multi_az             = false
+  skip_final_snapshot  = true
+}
