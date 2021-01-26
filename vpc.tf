9da91adec3a61df86f35412eaf765dc62ae2f5a6
Júlio Ballot <julio.ballot@gmail.com>
2021-01-26 11:16:23 -0300

    Adding vpc.tf file
    This file content allows the creation of a new VPC with a public and
    private network, where the public has a internet gateway configuration.
    The idea here is to have frontend and load balancers in the public
    subnet while keeping the backend and database away from the internet
    access but reachable by public services.

---
+++ vpc.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "mainvpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "main" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
