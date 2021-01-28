f7029b2c73d317f4d14d1a496e7b6a2d626f0f7f
Júlio Ballot <julio.ballot@gmail.com>
2021-01-28 01:59:08 -0300

    Adding NAT Gateway to the private subnets

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "available" {
+
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
@@ -9,22 +13,69 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
+  count = var.az_count
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnets
+  cidr_block              = var.public_subnets[count.index]
   map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 resource "aws_subnet" "private" {
+  count = var.az_count
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnets
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
 resource "aws_route" "main" {
-  route_table_id         = aws_vpc.main.main_route_table_id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = var.az_count
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+##
+## Creating NAT GW for private network internet access
+##
+resource "aws_eip" "gw" {
+  count = var.az_count
+  vpc = true
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_nat_gateway" "gw" {
+  count = var.az_count
+  subnet_id = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.gw[count.index].id
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "private" {
+  count = var.az_count
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw[count.index].id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = var.az_count
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
