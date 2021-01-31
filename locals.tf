18705ffb8b2bfa3a51f6a9f051c4bd47a2c1b050
Júlio Ballot <julio.ballot@4linux.com.br>
2021-01-30 23:31:08 -0300

    Adding two extra script: one for loading the postgres dump and the other for building and pushing docker image
    This two script are separated in load_postgres_dump.sh and
    build_push_docker_image.sh. The first will do a simple connection to the
    RDS PostgreSQL address, create a rates table and load the rates.sql dump
    file. The second one will build a new image, tag it and push to the ECR
    repository. One important thing, the triggers here configured for the
    null_resource is the current timestamp, which will make both scripts to
    run every time terraform apply is called.

--- locals.tf
+++ locals.tf
@@ -2,6 +2,27 @@
 ## Local_file resource, used in order to create the config.py for our rates app
 ##
 resource "local_file" "local" {
-  content = format("DB = { 'name': 'postgres', 'user': 'postgres', 'host': '%s'}", aws_db_instance.main.endpoint)
+  content = format("DB = { 'name': '%s', 'user': '%s', 'password': '%s','host': '%s'}", var.rds_database_name, var.rds_username, var.rds_password,aws_db_instance.main.address)
   filename = "rates/config.py"
+}
+
+##
+## null_resource used in order to run the script which will:
+## - Load database dumps.
+## - Build and push Docker image to ECR with latest tag
+##
+resource "null_resource" "load_data" {
+
+  triggers = {
+    always_run = format(timestamp())
+  }
+
+  provisioner "local-exec" {
+    command =  format("./load_postgres_dump.sh %s %s %s %s %s", aws_db_instance.main.address, aws_db_instance.main.port, var.rds_username, var.rds_password, var.rds_database_name )
+  }
+
+  provisioner "local-exec" {
+    command = format("./build_push_docker_image.sh %s", aws_ecr_repository.main.repository_url)
+  }
+
 }
