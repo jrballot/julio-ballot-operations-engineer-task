5ae90ae72b89830eed2ce9fb7747d75b3e7fa246
Júlio Ballot <julio.ballot@gmail.com>
2021-01-31 22:36:25 -0300

    Removing NAT Gateway from the vpc
    This change will only remove the NAT Gateway not used

--- vpc.tf
+++ vpc.tf
@@ -48,34 +48,3 @@ resource "aws_route_table_association" "public" {
 }
 
 
-##
-## Creating NAT GW for private network internet access
-##
-resource "aws_eip" "gw" {
-  count = var.az_count
-  vpc = true
-  depends_on = [aws_internet_gateway.main]
-}
-
-resource "aws_nat_gateway" "gw" {
-  count = var.az_count
-  subnet_id = aws_subnet.public[count.index].id
-  allocation_id = aws_eip.gw[count.index].id
-  depends_on = [aws_internet_gateway.main]
-}
-
-resource "aws_route_table" "private" {
-  count = var.az_count
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.gw[count.index].id
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  count = var.az_count
-  subnet_id = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
-}
